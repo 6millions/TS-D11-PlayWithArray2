789ac0814d8e17fbdda18a55ab96013161d29d78
6millions <kittichai.s@ic-connection.com>
2024-07-20 00:49:46 +0700

    add initial logic

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,22 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+function executeArray(): string {
+
+    interface Person {
+        name: string;
+        age: number;
+        lottos: number[];
+        numberOfLottos: number;
+
+    }
+
+    let numOfPerson: number;
+
+    numOfPerson = 10;
+
+    // create person array object basefd on numOfPerson
+    return "";
+}

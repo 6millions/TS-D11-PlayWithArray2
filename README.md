facaa51af0f0a5d813ecf9a063881b3db33de417
6millions <kittichai.s@ic-connection.com>
2024-07-20 16:27:18 +0700

    finish practice array chapter 2

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# TS-Challenge-Template
-Template of TS Challenge
+# TS-D11-PlayWithArray2
+Practice Array In TypeScript
+
+## Overview
+Practice Array In TypeScript
+
+## Installation
+1. Clone the repository
+2. Install dependencies: npm install
+
+## Problem Explanation
+1. Create Object Type which includes the array
+2. Create people
+3. Define a digits of lottery (randome how many lottos)
+4. Generate lotto number for each person
+
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+# Special Note
+Include `launch.json` to version control, for the purpose of this project and as a learning resource for debugging TypeScript projects with Visual Studio Code.
--- src/index.ts
+++ src/index.ts
@@ -8,27 +8,88 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
+
+    const peopleList = executeArray();
+
+    res.json(peopleList);
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+interface Person {
+    name: string;
+    age: number;
+    lottos: number[];
+    numberOfLottos: number;
 
-function executeArray(): string {
+}
 
-    interface Person {
-        name: string;
-        age: number;
-        lottos: number[];
-        numberOfLottos: number;
+function executeArray(): Person[] {
+
+    console.log("[executeArray]");
+
+    let peopleList: Person[] = [];
+
+    let numOfPeople: number;
+
+    numOfPeople = 10;
+
+    // create 10 people
+    peopleList = createPeople(numOfPeople);
+
+
+    // generate lotto based on random number of lottos
+
+    peopleList = generateLotto(peopleList);
+
+
+    console.log(peopleList);
+
+    // create person array object basefd on numOfPerson
+    return peopleList;
+}
+
+function createPeople(peopleNo: number): Person[] {
+
+    console.log("[createPeople]");
+
+    const peopleList: Person[] = [];
+
+    for (let i = 0; i < peopleNo; i++) {
+        const person: Person = {
+            name: "a" + i,
+            age: randomNumber(60),
+            lottos: [],
+            numberOfLottos: randomNumber(10),
+
+        }
+
+        peopleList.push(person);
 
     }
 
-    let numOfPerson: number;
+    return peopleList;
+}
 
-    numOfPerson = 10;
+function generateLotto(peopleList: Person[]): Person[] {
 
-    // create person array object basefd on numOfPerson
-    return "";
+    for (let i = 0; i < peopleList.length; i++) {
+
+        for (let j = 0; j < peopleList[i].numberOfLottos; j++) {
+
+            peopleList[i].lottos.push(randomNumber(99));
+        }
+
+
+    }
+    return peopleList;
+}
+
+function randomNumber(range: number): number {
+
+    console.log("[randomNumber]");
+    const ranNumber = Math.floor(Math.random() * range) + 1;
+
+    return ranNumber;
 }
